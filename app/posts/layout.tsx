bca5d10693b392af5147ac545e7df79bc406b932
daeuneeee <dxexx2@gmail.com>
2023-07-06 23:42:23 +0900

    layout-sidebar 수정 - 필요없는 페이지에서 숨김 처리

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -1,35 +1,41 @@
-'use client'
+'use client';
 
-import Header from '@components/header/Header'
-import Sidebar from '@components/sidebar/Sidebar'
-import styled from '@emotion/styled'
+import Header from '@components/header/Header';
+import Sidebar from '@components/sidebar/Sidebar';
+import styled from '@emotion/styled';
+import { usePathname } from 'next/navigation';
 
 interface IPostsLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 const PostsLayout = ({ children }: IPostsLayoutProps) => {
+  const pathName = usePathname();
+  const HIDDEN_SIDEBAR = ['/posts/new'];
+
+  const hiddenSidebar = HIDDEN_SIDEBAR.includes(pathName);
+
   return (
     <html lang="ko">
       <Body>
         <Header />
         <MainContainer>
-          <Sidebar />
+          {!hiddenSidebar && <Sidebar />}
           {children}
         </MainContainer>
       </Body>
     </html>
-  )
-}
+  );
+};
 
-export default PostsLayout
+export default PostsLayout;
 
 const Body = styled.body`
   padding: 0 10rem;
-`
+`;
 
 const MainContainer = styled.div`
   display: flex;
   flex-direction: row;
   margin-top: 3rem;
-`
+`;
